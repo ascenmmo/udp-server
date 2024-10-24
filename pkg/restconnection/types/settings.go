f4a81fca897562d04657e7f07b2c00995eb9ec17
temur abdurahmanov <ascemmephone@gmail.com>
2024-10-25 00:22:48 +0300

    fix settings

--- pkg/restconnection/types/settings.go
+++ pkg/restconnection/types/settings.go
@@ -8,8 +8,8 @@ import (
 
 type Settings struct {
 	ServerType          string `json:"serverType"`
-	TCPPort             string `json:"tcpPort"`
-	ServerPort          string `json:"serverPort"`
+	ServerPort          string `json:"tcpPort"`
+	ConnectionPort      string `json:"serverPort"`
 	ServerAddress       string `json:"serverAddress"`
 	MaxConnections      int    `json:"maxConnections"`
 	MaxRequestPerSecond int    `json:"maxRequestPerSecond"`
@@ -17,8 +17,8 @@ type Settings struct {
 
 func NewSettings() (settings Settings) {
 	settings.ServerType = "upd"
-	settings.TCPPort = env.TCPPort
-	settings.ServerPort = env.UDPPort
+	settings.ServerPort = env.TCPPort
+	settings.ConnectionPort = env.UDPPort
 	settings.ServerAddress = env.ServerAddress
 	settings.MaxConnections = CountConnectionsMAX()
 	settings.MaxRequestPerSecond = env.MaxRequestPerSecond
